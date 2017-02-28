6c3bf245a1f6262fec55df02180ce4243db767b3
Warner Losh <imp@FreeBSD.org>
2017-02-28 23:42:47 +0000

    Renumber copyright clause 4
    Renumber cluase 4 to 3, per what everybody else did when BSD granted
    them permission to remove clause 3. My insistance on keeping the same
    numbering for legal reasons is too pedantic, so give up on that point.
    
    Submitted by:	Jan Schaumann <jschauma@stevens.edu>
    Pull Request:	https://github.com/freebsd/freebsd/pull/96

--- libc/gen/pw_scan.h
+++ libc/gen/pw_scan.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1990, 1993, 1994
+ * Copyright (c) 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,38 +25,12 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)pw_scan.h	8.1 (Berkeley) 4/1/94
+ * $FreeBSD$
  */
 
-#if 0
-#ifndef lint
-static const char sccsid[] = "@(#)table.c	8.3 (Berkeley) 4/2/94";
-#endif /* not lint */
-#endif
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+#define _PWSCAN_MASTER 0x01
+#define _PWSCAN_WARN   0x02
 
-#include <sys/types.h>
-#include <stddef.h>
-#include "chpass.h"
-
-ENTRY list[] = {
-	{ "login",		p_login,  1,   5, ": ", NULL },
-	{ "password",		p_passwd, 1,   8, ": ", NULL },
-	{ "uid",		p_uid,    1,   3, ": ", NULL },
-	{ "gid",		p_gid,    1,   3, ": ", NULL },
-	{ "class",		p_class,  1,   5, ": ", NULL },
-	{ "change",		p_change, 1,   6, NULL, NULL },
-	{ "expire",		p_expire, 1,   6, NULL, NULL },
-#ifdef RESTRICT_FULLNAME_CHANGE		/* do not allow fullname changes */
-	{ "full name",		p_gecos,  1,   9, ":,", NULL },
-#else
-	{ "full name",		p_gecos,  0,   9, ":,", NULL },
-#endif
-	{ "office phone",	p_gecos,  0,  12, ":,", NULL },
-	{ "home phone",		p_gecos,  0,  10, ":,", NULL },
-	{ "office location",	p_gecos,  0,  15, ":,", NULL },
-	{ "other information",	p_gecos,  0,  11, ": ", NULL },
-	{ "home directory",	p_hdir,   1,  14, ": ", NULL },
-	{ "shell",		p_shell,  0,   5, ": ", NULL },
-	{ NULL, NULL, 0, 0, NULL, NULL },
-};
+extern int	__pw_scan(char *, struct passwd *, int);
